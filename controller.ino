c4f045135f89ba7628e7f173a2c7b4c0afeb1363
NERobotics <robotics.northeugene@gmail.com>
2023-04-27 17:00:38 -0700

    Create controller.ino
    Declared pins

---
+++ controller.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT); // x position of joystick
+  pinMode(A1, INPUT); // y position of joystick
+  pinMode(8, INPUT); // Button for emergency breaks
+  pinMode(7, INPUT); // Button for Lights
+  pinMode(3, INPUT); // Button for Speed Control (Turbo Mode)
+  pinMode(9, INPUT); // Button for Power Locking
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
